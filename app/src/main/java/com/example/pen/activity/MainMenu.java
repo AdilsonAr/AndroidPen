b5c74ffb8a6e9afb659b662f5feab578f78a0f35
AdilsonAr <cycles.250@gmail.com>
2021-04-21 16:24:21 -0600

    diseño de iconos y menu

--- app/src/main/java/com/example/pen/activity/MainMenu.java
+++ app/src/main/java/com/example/pen/activity/MainMenu.java
@@ -1,7 +1,12 @@
 package com.example.pen.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
 import com.example.pen.R;
 public class MainMenu extends AppCompatActivity {
 
@@ -9,5 +14,13 @@ public class MainMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+        ImageButton back=findViewById(R.id.back);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent act=new Intent(MainMenu.this,MainActivity.class);
+                startActivity(act);
+            }
+        });
     }
 }
